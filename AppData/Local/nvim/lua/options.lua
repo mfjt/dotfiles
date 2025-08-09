6fbb4b0744024d4c7e36b26230d3720abd750fbd
oɹıɥɐsɐɯ ɐʇıɾnɟ <mfjt@users.noreply.github.com>
2025-08-10 03:47:36 +0900

    Neovim設定更新

--- AppData/Local/nvim/lua/options.lua
+++ AppData/Local/nvim/lua/options.lua
@@ -8,7 +8,6 @@ if not vim.g.vscode then
   vim.opt.cursorline = true
   vim.opt.listchars = { eol = "↲", tab = "»·", trail = "·", extends = "»", precedes = "«", nbsp = "%" }
   vim.opt.list = true
-  vim.opt.number = true
   vim.opt.relativenumber = true
   vim.opt.smartindent = true
 end
