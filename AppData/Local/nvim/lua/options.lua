beb0624b378d5eb31e8d4a174cb4942e680b8700
oɹıɥɐsɐɯ ɐʇıɾnɟ <mfjt@users.noreply.github.com>
2025-08-10 18:50:08 +0900

    Neovim設定変更

--- AppData/Local/nvim/lua/options.lua
+++ AppData/Local/nvim/lua/options.lua
@@ -1,13 +1,19 @@
-vim.opt.clipboard = 'unnamedplus'
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.swapfile = false
-vim.opt.undofile = true
+
+vim.opt.clipboard = 'unnamedplus'
 
 if not vim.g.vscode then
+  vim.opt.backup = true
+  vim.opt.backupdir:remove(".")
+  vim.opt.undofile = true
+
   vim.opt.cursorline = true
-  vim.opt.listchars = { eol = "↲", tab = "»·", trail = "·", extends = "»", precedes = "«", nbsp = "%" }
   vim.opt.list = true
+  vim.opt.listchars = { eol = "↲", tab = "»·", trail = "·", extends = "»", precedes = "«", nbsp = "%" }
+  vim.opt.number = true
   vim.opt.relativenumber = true
   vim.opt.smartindent = true
+else
+  vim.opt.swapfile = false
 end
