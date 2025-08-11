24e98cc02bd8591752648b6eed1c0128d1c1fef4
oɹıɥɐsɐɯ ɐʇıɾnɟ <mfjt@users.noreply.github.com>
2025-08-11 13:27:49 +0900

    keymap設定を追加し、不要な設定を整理

--- AppData/Local/nvim/lua/keymaps.lua
+++ AppData/Local/nvim/lua/keymaps.lua
@@ -1,6 +1,14 @@
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { silent = true })
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "s", '"_s')
+vim.keymap.set("n", "S", '"_S')
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("n", "X", '"_X')
+vim.keymap.set("v", "s", '"_s')
+vim.keymap.set("v", "S", '"_S')
+vim.keymap.set("v", "x", '"_x')
+vim.keymap.set("v", "X", '"_X')
 
 if not vim.g.vscode then
   vim.keymap.set("i", "jj", "<Esc>")
