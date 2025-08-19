4b5094d8fd1b257194124a074e3715f64f0db695
oɹıɥɐsɐɯ ɐʇıɾnɟ <mfjt@users.noreply.github.com>
2025-08-19 22:08:31 +0900

    keymapの設定を修正し、jおよびkの動作を条件付きで設定

--- AppData/Local/nvim/lua/keymaps.lua
+++ AppData/Local/nvim/lua/keymaps.lua
@@ -1,6 +1,4 @@
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { silent = true })
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
 vim.keymap.set("n", "s", '"_s')
 vim.keymap.set("n", "S", '"_S')
 vim.keymap.set("n", "U", "<C-r>")
@@ -14,4 +12,9 @@ vim.keymap.set("v", "X", '"_X')
 
 if not vim.g.vscode then
   vim.keymap.set("i", "jj", "<Esc>")
+  vim.keymap.set("n", "j", 'v:count == 0 ? "gj" : "j"', { expr = true })
+  vim.keymap.set("n", "k", 'v:count == 0 ? "gk" : "k"', { expr = true })
+else
+  vim.keymap.set("n", "j", 'v:count == 0 ? "gj" : "j"', { expr = true, remap = true })
+  vim.keymap.set("n", "k", 'v:count == 0 ? "gk" : "k"', { expr = true, remap = true })
 end
